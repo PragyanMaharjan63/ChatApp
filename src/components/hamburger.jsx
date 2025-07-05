6d213e3342486c0f63f4a7ff982093fd563719c0
PragyanMaharjan63 <pragyanmaharjan6k@gmail.com>
2025-07-05 22:43:59 +0545

    small changes

--- src/components/hamburger.jsx
+++ src/components/hamburger.jsx
@@ -5,7 +5,7 @@ import Navbar from "./navbar";
 export default function hamburger() {
   const [navbar, showNav] = useState(false);
   return (
-    <div>
+    <>
       <div className="flex  relative w-40 left-0 -top-1.5 bg-neutral-800 rounded-lg">
         <div
           className="absolute top-0"
@@ -18,6 +18,6 @@ export default function hamburger() {
         </div>
         <div>{navbar && <Navbar />}</div>
       </div>
-    </div>
+    </>
   );
 }
